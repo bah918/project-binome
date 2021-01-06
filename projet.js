403631cd3842826f041570d9f3650d3acb5c3e73
pknowledge <nafiou6@yahoo.fr>
2021-01-06 12:21:47 +0100

    ajout fonction droite

--- projet.js
+++ projet.js
@@ -15,3 +15,18 @@ var grid = [
 var rover = {
     direction:"N",
 };
+function turnRight(){
+    rover={direction:"E"}
+    console.log("Changement de direction!");
+    console.log(rover);
+}
+
+
+function turnRight(){
+    rover={direction:"E"}
+    console.log("Changement de direction!");
+    console.log(rover);
+}
+console.log(rover);
+turnRight();
+
