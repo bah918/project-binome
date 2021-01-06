1a53ca353df5ab86857cb658b52f4733e88b3572
SNarboni <sebastien.narboni@hotmail.fr>
2021-01-06 16:33:38 +0100

    ajout de la fonction pilotRover

--- projet.js
+++ projet.js
@@ -17,13 +17,14 @@ var rover = {
     direction: "N",
     x: 0,
     y: 0,
+    travelLog: [],
 };
 
 // fonction pour se tourner a gauche
 function turnLeft(rover) {
     if (rover.direction === "N") {
-        rover.direction = "W";
-    } else if (rover.direction === "W") {
+        rover.direction = "O";
+    } else if (rover.direction === "O") {
         rover.direction = "S";
     } else if (rover.direction === "S") {
         rover.direction = "E";
@@ -32,20 +33,6 @@ function turnLeft(rover) {
     }
     console.log(rover);
 }
-
-// fonction pour tourner a droite
-// function turnRight(rover) {
-//     if (rover.direction === "N") {
-//         rover.direction = "E";
-//     } else if (rover.direction === "E") {
-//         rover.direction = "S";
-//     } else if (rover.direction === "S") {
-//         rover.direction = "W";
-//     } else if (rover.direction === "W") {
-//         rover.direction = "N";
-//     }
-//     console.log(rover);
-// }
 
 function turnRight(rover) {
     switch (rover.direction) {
@@ -56,9 +43,9 @@ function turnRight(rover) {
             rover.direction = "S";
             break;
         case "S":
-            rover.direction = "W";
+            rover.direction = "O";
             break;
-        case "W":
+        case "O":
             rover.direction = "N";
             break;
     }
@@ -72,15 +59,29 @@ function moveForward(rover) {
         rover.y = rover.y - 1;
     } else if (rover.direction === "E") {
         rover.x += 1;
-    } else if (rover.direction === "W") {
+    } else if (rover.direction === "O") {
         rover.x = rover.x - 1;
     }
     console.log(rover)
 }
 
-console.log(rover);
-turnLeft(rover);// tourne de 45deg vers la gauche
-turnLeft(rover);// tourne de 45deg vers la gauche
-moveForward(rover)
-turnLeft(rover);// tourne de 45deg vers la gauche
-moveForward(rover)
+function pilotRover(string) {
+    var mouvement = [];
+    mouvement = string.toUpperCase().split("");
+    for (var i = 0; i < mouvement.length; i++) {
+        if (mouvement[i] === "L") {
+            turnLeft(rover);
+            console.log("+1 à gauche");
+        } else if (mouvement[i] === "R") {
+            turnRight(rover);
+            console.log("+1 à droite");
+        } else if (mouvement[i] === "F") {
+            moveForward(rover)
+        } else {
+            console.log("je sais pas !")
+        }
+    }
+    console.log(mouvement)
+}
+
+pilotRover("Rrrff");
